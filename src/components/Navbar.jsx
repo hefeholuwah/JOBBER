92eedeff80b6380c7c40b51fca064ac2395cdf36
Vulcanric <jneric49@gmail.com>
2024-10-08 02:20:03 +0100

    Update: Added more navigation routes, logo and new styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,42 @@
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
+import SignupButton from './SignupButton';
 
 const Navbar = () => {
   const linkClass = ({ isActive }) =>
     isActive
-      ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-      : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+      ? 'font-extrabold bg-white text-black hover:bg-white hover:text-black rounded-md px-3 py-2'
+      : 'font-semibold text-white hover:bg-white hover:text-black rounded-md px-3 py-2';
 
   return (
-    <nav className='bg-indigo-700 border-b border-indigo-500'>
+    <nav className='bg-b position-fixed w-full border-b border-indigo-500'>
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='flex h-20 items-center justify-between'>
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
             <NavLink className='flex flex-shrink-0 items-center mr-4' to='/'>
-              <img className='h-10 w-auto' src={logo} alt='React Jobs' />
-              <span className='hidden md:block text-white text-2xl font-bold ml-2'>
-                React Jobs
+              <img className='filter h-10 w-auto' src={logo} alt='Remotely' />
+              <span className='hidden md:block text-white text-2xl ml-2'>
+                &lt;/&gt;
               </span>
             </NavLink>
-            <div className='md:ml-auto'>
+            <div className='mx-auto'>
               <div className='flex space-x-2'>
-                <NavLink to='/' className={linkClass}>
-                  Home
-                </NavLink>
                 <NavLink to='/jobs' className={linkClass}>
-                  Jobs
+                  Find a Job
                 </NavLink>
-                <NavLink to='/add-job' className={linkClass}>
-                  Add Job
+                <NavLink to='/companies' className={linkClass}>
+                  Companies
                 </NavLink>
-                <NavLink to='/select-user-type' className={linkClass}>
-                  Sign Up
+                <NavLink to='/talents' className={linkClass}>
+                  Talents
+                </NavLink>
+                <NavLink to='/employers' className={linkClass}>
+                  Employers
                 </NavLink>
               </div>
+            </div>
+            <div className='flex items-center justify-between'>
+              <SignupButton />
             </div>
           </div>
         </div>
