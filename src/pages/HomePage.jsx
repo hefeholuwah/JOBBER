3da7e35be0c2133f715e98e6ffa285ca238a2cc4
piintegrated <piintegratedservices1@gmail.com>
2024-10-11 16:13:45 +0100

    removed view all job from landing page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,17 @@
 // import navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import DevTypeCards from '../components/DevTypeCards';
-import JobListings from '../components/JobListings';
-import ViewAllJobs from '../components/ViewAllJobs';
-import { updatePageTitle } from '../utils';
+import Hero from "../components/Hero";
+import DevTypeCards from "../components/DevTypeCards";
+import JobListings from "../components/JobListings";
+import { updatePageTitle } from "../utils";
 
 const HomePage = () => {
   updatePageTitle("REMOTELYDEV | Find your Dream Job");
 
   return (
     <>
-    
       <Hero />
       <DevTypeCards />
       <JobListings isHome={true} />
-      <ViewAllJobs />
     </>
   );
 };
