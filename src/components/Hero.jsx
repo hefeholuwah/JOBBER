5cd8e3bb3f11941474c3ba14c3079e561c3f2795
Vulcanric <jneric49@gmail.com>
2024-10-12 12:35:06 +0100

    Made Hero components responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,18 @@
-import { useState } from "react";
 import JobUpdate from "./JobUpdate";
-import { realTimeJobs } from "../services/RealTimeJobService";
 
 const Hero = () => {
   return (
-    <section className='section w-full bg-black flex py-20 mb-4'>
+    <section className='w-full section bg-black flex py-20 mb-4'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center'>
         <div className='text-center'>
-          <h2 className="text-4xl font-semibold text-white my-4">
+          <h2 className="text-4xl font-semibold text-white my-4 sm:text-5xl md:text-6xl text-center">
             You've honed your skills, now
           </h2>
           <h1 className='text-bl font-extrabold text-white sm:text-5xl md:text-6xl'>
             Find your Dream Job
           </h1>
           <br />
-          <JobUpdate
-            jobs={realTimeJobs ? realTimeJobs.jobs : []}
-          />
+          <JobUpdate />
         </div>
       </div>
     </section>
